35a95cba9bd2c0c7c1c95dee7ecf848e1221659e
Dhananjay <161328822+Dhananjay1313@users.noreply.github.com>
2024-02-28 14:38:02 +0530

    Add files via upload

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,62 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</head>
+<style>
+    .container {
+        width: 1170px;
+        border: 3px solid aquamarine;
+        padding: 2%;
+        border-radius: 25px;
+    }
+</style>
+<body>
+    <div class="container mt-5">
+        <form  action="update" method="POST"> 
+            @csrf
+            @method('PUT')      
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Firstname:</label>
+                <input type="text" class="form-control" id="firstname" name="firstname" aria-describedby="emailHelp" value="{{ old('firstname', $person->firstname) }}">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Lastname:</label>
+                <input type="text" class="form-control" id="lastname" name="lastname" aria-describedby="emailHelp" value="{{ old('lastname', $person->lastname) }}">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email:</label>
+                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" value="{{ old('email', $person->email) }}">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password:</label>
+                <input type="password" class="form-control" id="password" name="password" value="{{ old('password', $person->password) }}">
+            </div>
+            <label for="exampleInputPassword1" class="form-label">Gender:</label>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="gender" id="male" value="male" {{ old('gender', $person->gender) === 'male' ? 'checked' : '' }}  >
+                <label class="form-check-label" for="flexRadioDefault1">
+                    Male
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="gender" id="female" value="female" {{ old('gender', $person->gender) === 'female' ? 'checked' : '' }}>
+                <label class="form-check-label" for="flexRadioDefault1">
+                    Female
+                </label>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Contact No:</label>
+                <input type="number" class="form-control" id="contact_no" name="contact_no" value="{{ old('contact_no', $person->contact_no) }}">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</body>
+</html>
---
+++ resources/views/form.blade.php
@@ -0,0 +1,93 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</head>
+<style>
+    .container {
+        width: 1170px;
+        border: 3px solid aquamarine;
+        padding: 2%;
+        border-radius: 25px;
+    }
+</style>
+<body>
+    <div class="container mt-5">
+        <form  action="/add"> 
+            @csrf
+            @method('PUT')     
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Firstname:</label>
+                <input type="text" class="form-control" id="firstname" name="firstname">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Lastname:</label>
+                <input type="text" class="form-control" id="lastname" name="lastname">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email:</label>
+                <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password:</label>
+                <input type="password" class="form-control" id="password" name="password">
+            </div>
+            <label for="exampleInputPassword1" class="form-label">Gender:</label>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="gender" id="male" value="male">
+                <label class="form-check-label" for="flexRadioDefault1">
+                    Male
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="gender" id="female" value="female">
+                <label class="form-check-label" for="flexRadioDefault1">
+                    Female
+                </label>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Contact No:</label>
+                <input type="number" class="form-control" id="contact_no" name="contact_no">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        <table class="table table-striped">
+            <tr>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th>Gender</th>
+                <th>Contact No.</th>
+                <th>Actions</th>
+            </tr>
+            <tbody>
+                @foreach($persons as $person)
+                <tr>
+                    <td>{{ $person->firstname }}</td>
+                    <td>{{ $person->lastname }}</td>
+                    <td>{{ $person->email }}</td>
+                    <td>{{ $person->password }}</td>
+                    <td>{{ $person->gender }}</td>
+                    <td>{{ $person->contact_no }}</td>
+                    <td>
+                        <a href="person/{{$person->id}}/edit" class="btn btn-info">Edit</a>
+                        <form action="person/{{$person->id}}/delete" method="POST" class="d-inline">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Delete</button>
+                          </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
